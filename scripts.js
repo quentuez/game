4e969112504fe1e25d667cd9f46f0b4f7725a163
quentuez <dmitrij.klimenkoff7@gmail.com>
2023-02-08 22:32:15 +0700

    Project initialized

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+class Player {
+  constructor() {
+    this.width = 240;
+    this.height = 104;
+    this.x = 20;
+    this.y = 100;
+  }
+}
